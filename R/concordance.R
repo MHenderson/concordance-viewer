23d98af69b861845e52bd74de660687e5f84d0d7
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-05-24 16:53:46 +0000

    use package prefixes in functions

--- R/concordance.R
+++ R/concordance.R
@@ -1,13 +1,13 @@
 concordance <- function(corpus, search_term, left = -50, right = 50) {
   x <- corpus %>%
-    corpus_frame() %>%
-    text_locate(search_term) %>%
-    mutate(
-      before = as.character(before) %>% str_sub(start = left),
+    corpus::corpus_frame() %>%
+    corpus::text_locate(search_term) %>%
+    dplyr::mutate(
+      before = as.character(before) %>% stringr::str_sub(start = left),
       instance = as.character(instance),
-      after = as.character(after) %>% str_sub(end = right)
+      after = as.character(after) %>% stringr::str_sub(end = right)
     )
   x$title <- as.factor(corpus$title[as.numeric(x$text)])
   x$author <- as.factor(corpus$author[as.numeric(x$text)])
-  return(x %>% select(title, before, instance, after, author))
+  return(x %>% dplyr::select(title, before, instance, after, author))
 }
